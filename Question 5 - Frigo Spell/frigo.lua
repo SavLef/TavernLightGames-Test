94c650a2e4d504e89b24f96bc46ee6770e17a5e8
Lefkos Savvides <39127178+SavLef@users.noreply.github.com>
2024-05-22 00:35:37 +0300

    Updated Q5
    - Added the particles from the example
    - Added new video
    - Extended the functionality of the spell by adding a spell loop counter

--- Question 5 - Frigo Spell/frigo.lua
+++ Question 5 - Frigo Spell/frigo.lua
@@ -1,50 +1,67 @@
+-- Define combat 1
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICEAREA)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 
--- For the second set of particles as seen in the video, I chose to do it as multiple combats
+-- Define combat 2
 local combat2 = Combat()
-combat2:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
-combat2:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
+combat2:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat2:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 
--- Define a custom area shape as a table
+-- Define how many times to cast each spell
+local spellLoop = 10
+
+-- Define custom area shapes as tables
 local customArea = {
-    {0, 1, 0, 1, 0},
-    {0, 0, 0, 0, 0},
-    {1, 1, 3, 1, 1},
-    {0, 0, 0, 0, 0},
-    {0, 1, 0, 1, 0}
-}
-local customArea2 = {
-    {0, 0, 1, 0, 0},
-    {0, 1, 1, 1, 0},
-    {0, 0, 3, 0, 0},
-    {0, 1, 1, 1, 0},
-    {0, 0, 1, 0, 0}
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 2, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0}
 }
 
--- Use the custom area shape with createCombatArea
+local customArea2 = {
+    {0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1, 0, 0},
+    {0, 0, 0, 0, 2, 0, 0, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0}
+}
+
+-- Use the custom area shapes with createCombatArea
 combat:setArea(createCombatArea(customArea))
 combat2:setArea(createCombatArea(customArea2))
 
+--Calculate the values to be used for damage for combat 1
 function onGetFormulaValues1(player, level, magicLevel)
     local min = (level / 5) + (magicLevel * 0.8) + 5
     local max = (level / 5) + (magicLevel * 2) + 12
     return -min, -max
 end
 
--- The second part of the spell will do less damage (random values)
+--Calculate the values to be used for damage for combat 2
 function onGetFormulaValues2(player, level, magicLevel)
     local min = (level / 7) + (magicLevel * 0.8) + 5
     local max = (level / 7) + (magicLevel * 2) + 12
     return -min, -max
 end
 
-combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+--Calculates the damage value for combat 1
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues1")
+--Calculates the damage value for combat 2
 combat2:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues2")
 
+--Casts both spells defined number of times
 function onCastSpell(creature, variant)
-    local result1 = combat:execute(creature, variant)
-    local result2 = combat2:execute(creature, variant)
-    return result1 and result2
+    for i = 1, spellLoop do
+        combat:execute(creature, variant)
+        combat2:execute(creature, variant)
+    end
+    return true
 end
